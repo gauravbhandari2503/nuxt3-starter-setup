a02908ae0aa1a1e0e0e3133f95668c93e92dc021
Gaurav.Bhandari <gaurav2503bhandari@gmail.com>
2024-01-15 15:48:40 +0530

    practised nuxt concepts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import {resolve} from "path";
 export default defineNuxtConfig({
+  // page wise if you want to add titles or meta data, you can use the useHead composable.
+  app: {
+    head: {
+      title: "Nuxt course",
+      meta: [
+        {
+          name: "description",
+          content: "This a nuxt course from Gulliaume"
+        }
+      ]
+    }
+  },
   devtools: { enabled: true },
   alias: {
-    "@": resolve(__dirname, "/")
+    "@": new URL('./', import.meta.url).pathname,
   },
   buildModules: ['@nuxt/typescript-build'],
   css: ["~/assets/main.scss"],
@@ -13,4 +24,11 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  runtimeConfig: {
+    public: {
+      MY_ENV_VARIABLE: process.env.MY_ENV_VARIABLE,
+      API_ROOT: process.env.API_ROOT
+    },
+  },
+  ssr: false
 })
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,18 @@
     const {sayWelcome, sayGoodbye} = useUtils();
     sayWelcome();
     sayGoodbye();
+    const resp = await $fetch('/api/hello');
+    // useFetch can be used to fetch data during the server-side rendering (SSR) process.
+    // It will return the data immediately if it is already available, otherwise it will wait for the data to be fetched before rendering the page.
+
+    // useLazyFetch is similar to useFetch, but it is used to fetch data during the client-side rendering (CSR) process.
+    // It will only fetch the data when the component is mounted and will not block the rendering of the page.
+    const {data: resp1} = await useFetch('/api/hello');
+    console.log(resp1.value);
+    const runTimeConfig = useRuntimeConfig();
+    console.log(runTimeConfig.public.API_ROOT);
 </script>
 <template>
+    <pre><code>{{ resp }}</code></pre>
     <h1>Default Page</h1>
 </template>
